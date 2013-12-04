30cd9400a5a310c5d4d48cb810ec20b79e6769a4
Scyntrus <Scyntrus@gmail.com>
2013-12-04 00:30:33 -0800

    Added InventoryClickEvent

--- src/com/bol/ListenerSoulbind.java
+++ src/com/bol/ListenerSoulbind.java
@@ -1,8 +1,10 @@
 package com.bol;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
@@ -16,21 +18,42 @@ public class ListenerSoulbind implements Listener {
 
 	@EventHandler
 	public void onInventoryMoveItem(InventoryMoveItemEvent e) {
-		if (e.getItem().getItemMeta().hasLore())
-			if (e.getItem().getItemMeta().getLore().contains("soulbound")){
-				if (e.getInitiator().getHolder() instanceof Player) {
-					((Player)e.getInitiator().getHolder()).sendMessage("That item is soulbound");
+		if (e.getItem().hasItemMeta())
+			if (e.getItem().getItemMeta().hasLore())
+				if (e.getItem().getItemMeta().getLore().contains("soulbound")) {
+					if (e.getSource().getHolder() instanceof Player) {
+						((Player) e.getInitiator().getHolder())
+								.sendMessage("That item is soulbound");
+					}
+					e.setCancelled(true);
 				}
-				e.setCancelled(true);
-			}
+	}
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		if (e.getInventory().getHolder().toString().equals(e.getWhoClicked().toString()))
+			return;
+		if (e.getCurrentItem() == null)
+			return;
+		if (!e.getCurrentItem().hasItemMeta())
+			return;
+		if (!e.getCurrentItem().getItemMeta().hasLore())
+			return;
+		if (!e.getCurrentItem().getItemMeta().getLore().contains("soulbound"))
+			return;
+		e.setResult(Result.DENY);
+		((Player) e.getWhoClicked()).sendMessage("That item is soulbound");
+		e.setCancelled(true);
 	}
 
 	@EventHandler
 	public void onPlayerDropItem(PlayerDropItemEvent e) {
-		if (e.getItemDrop().getItemStack().getItemMeta().hasLore())
-			if (e.getItemDrop().getItemStack().getItemMeta().getLore().contains("soulbound")){
-				e.getPlayer().sendMessage("That item is soulbound");
-				e.setCancelled(true);
-			}
+		if (e.getItemDrop().getItemStack().hasItemMeta())
+			if (e.getItemDrop().getItemStack().getItemMeta().hasLore())
+				if (e.getItemDrop().getItemStack().getItemMeta().getLore()
+						.contains("soulbound")) {
+					e.getPlayer().sendMessage("That item is soulbound");
+					e.setCancelled(true);
+				}
 	}
 }
